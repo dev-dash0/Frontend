b7f0b11ffa33e50b74bfbc36da7cdfc076d4b772
Shahd Mostafa <shahd.mustafa124@gmail.com>
2024-11-30 23:33:40 +0200

    try to resolve

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-GFRNXFPM.mjs";import"./chunk-7JZWJ2DF.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-X6NLUOBZ.mjs";import"./chunk-7JZWJ2DF.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
