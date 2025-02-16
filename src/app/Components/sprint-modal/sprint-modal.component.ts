ab15ead72e80be600eac5a2a926ec9d2f9d1597a
Rana-ElFetyany <ranafet777@gmail.com>
2025-02-16 02:38:05 +0200

    Creation of Sprint Modal

---
+++ src/app/Components/sprint-modal/sprint-modal.component.ts
@@ -0,0 +1,93 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import {
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { NgFor } from '@angular/common';
+import { MatOptionModule } from '@angular/material/core';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+export interface Label {
+  name: string;
+}
+
+@Component({
+  selector: 'app-sprint-modal',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    FormsModule,
+    MatSelectModule,
+    NgFor,
+    MatOptionModule,
+    MatChipsModule,
+    MatButtonToggleModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './sprint-modal.component.html',
+  styleUrl: './sprint-modal.component.css',
+})
+export class SprintModalComponent {
+  constructor(
+    private dialogRef: MatDialogRef<SprintModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private fb: FormBuilder
+  ) {}
+
+  SprintForm = new FormGroup({
+    title: new FormControl(null, Validators.required),
+    description: new FormControl(null),
+    status: new FormControl(null, Validators.required),
+    priority: new FormControl(null, Validators.required),
+  });
+
+  close() {
+    this.dialogRef.close();
+  }
+
+  selectedType: string = '';
+
+  // status
+  selectedStatus = 'backlog'; // Default selected status
+
+  statuses = [
+    {
+      value: 'planing',
+      label: 'Planning',
+      icon: 'assets/images/Issue Status/todo.svg',
+    },
+    {
+      value: 'in-progress',
+      label: 'In Progress',
+      icon: 'assets/images/Issue Status/in-progress.svg',
+    },
+    {
+      value: 'completed',
+      label: 'Completed',
+      icon: '../../assets/images/Issue Status/Completed.svg',
+    },
+  ];
+
+  
+  // Submit Form
+  submitForm() {
+    if (this.SprintForm.valid) {
+      console.log('Form submitted:', this.SprintForm.value);
+    }
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
 import { IssueModalComponent } from './Components/issue-modal/issue-modal.component';
 import { ProjectModalComponent } from './Components/project-modal/project-modal.component';
+import { SprintModalComponent } from './Components/sprint-modal/sprint-modal.component';
+import { AddCompanyModalComponent } from "./Components/company-modal/company-modal.component";
 
 @Component({
   selector: 'app-root',
@@ -17,7 +19,8 @@ import { ProjectModalComponent } from './Components/project-modal/project-modal.
     MatButtonModule,
     MatDividerModule,
     MatIconModule,
-  ],
+    AddCompanyModalComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -43,6 +46,20 @@ export class AppComponent {
   // ! Project Modal //////////////////////////////////////////////////////////////////
   openProj() {
     this.dialog.open(ProjectModalComponent, {
+      width: 'auto',
+      minWidth: '50vw',
+      maxWidth: '80vw', // Limits width to 90% of viewport
+      minHeight: '70vh',
+      maxHeight: '90vh', // Prevents excessive height
+      panelClass: 'custom-dialog-container', // Custom class for styling
+      disableClose: true,
+      data: { message: 'Hello from modal!' }, // ✅ Pass data to modal
+    });
+  }
+
+  // ! Sprint Modal //////////////////////////////////////////////////////////////////
+  openSprint() {
+    this.dialog.open(SprintModalComponent, {
       width: 'auto',
       minWidth: '50vw',
       maxWidth: '80vw', // Limits width to 90% of viewport
