b25cb2f41fc783979106255f1e6e5bf34c0e8c09
Rana-ElFetyany <ranafet777@gmail.com>
2025-05-15 00:59:40 +0300

    Email check and send OTP

--- src/app/Components/forget-password/forget-password.component.ts
+++ src/app/Components/forget-password/forget-password.component.ts
@@ -1,32 +1,101 @@
-import { Component } from '@angular/core';
+import { Component, inject, SimpleChanges } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
-import { NgClass } from '@angular/common';
+import { CommonModule, NgClass } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { MainDarkBtnComponent } from "../../Shared/main-dark-btn/main-dark-btn.component";
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { signupValidators } from '../../Shared/validators/validators.component';
+import { ForgetPasswordService } from '../../Core/Services/forget-password.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-forget-password',
   standalone: true,
   imports: [
+    CommonModule,
     RouterLink,
     MatToolbarModule,
     MatButtonModule,
     MatDividerModule,
     MatIconModule,
-    NgClass,
-    MainDarkBtnComponent
+    MainDarkBtnComponent,
+    ReactiveFormsModule,
   ],
   templateUrl: './forget-password.component.html',
   styleUrl: './forget-password.component.css',
 })
 export class ForgetPasswordComponent {
-  steps: number = 4;
+  steps: number = 1;
+
+  private readonly _FormBuilder = inject(FormBuilder);
+  private readonly _ForgetPassService = inject(ForgetPasswordService);
+  private readonly _toaster = inject(ToastrService);
+
+  ForgetForm: FormGroup = this._FormBuilder.group({
+    email: [null, Validators.required],
+  });
+
+  OTPForm: FormGroup = this._FormBuilder.group({
+    token: [null, Validators.required],
+  });
+
+  ResetForm: FormGroup = this._FormBuilder.group({
+    newPassword: [null, Validators.required],
+  });
+
+  onSubmit(event: Event): void {
+    event.preventDefault();
+  }
+
+  ForgetPassword() {
+    // if (this.ForgetForm.valid) {
+    this._ForgetPassService.ForgotPassword(this.ForgetForm.value).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.navigateVerify();
+        this.showSuccess(res.message);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+    // }
+  }
+
+  VerifyOTP() {
+    // if (this.ForgetForm.valid) {
+    this._ForgetPassService.VerifyOTP(this.OTPForm.value).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.navigateReset();
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+    // }
+  }
+
+  ResetPassword() {
+    // if (this.ForgetForm.valid) {
+    this._ForgetPassService.ResetPassword(this.ResetForm.value).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.DonePass();
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+    // }
+  }
 
   navigateForgot = () => {
     this.steps = 1;
+    localStorage.setItem('currentStep', this.steps.toString());
   };
 
   navigateVerify = () => {
@@ -51,11 +120,28 @@ export class ForgetPasswordComponent {
     // }
   };
 
+  showSuccess(msg: string) {
+    this._toaster.success(msg, 'Success Message', {
+      toastClass: 'toast-pink',
+      timeOut: 5000,
+      closeButton: true,
+      progressBar: true,
+      progressAnimation: 'decreasing',
+    });
+  }
+
   ngOnInit(): void {
-    this.steps = 4;
-    this.steps = parseInt(localStorage.getItem('currentStep') || '1');
+    localStorage.setItem('currentStep', this.steps.toString());
+    // this.steps = parseInt(localStorage.getItem('currentStep') || '1');
     // this.resetPassword
     //   .get('email')
     //   ?.setValue(localStorage.getItem('currentEmail'));
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    this.steps = parseInt(localStorage.getItem('currentStep')! || '1');
+    localStorage.setItem('currentStep', this.steps.toString());
   }
 }
