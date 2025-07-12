06f2a3db2b129c1f637053a81b745ee170b2bb89
Rana-ElFetyany <ranafet777@gmail.com>
2025-07-12 03:53:35 +0300

    loading at all-projects page

--- src/app/Components/all-projects/all-projects.component.ts
+++ src/app/Components/all-projects/all-projects.component.ts
@@ -1,22 +1,22 @@
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { Company } from '../../Core/interfaces/company/company';
-import { MatTabsModule } from '@angular/material/tabs';
-import { Component, Inject, inject } from '@angular/core';
-import { SidebarService } from '../../Core/Services/sidebar.service';
+import { MatDialog } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatChipsModule } from '@angular/material/chips';
-import { AllcompaniescardComponent } from '../allcompaniescard/allcompaniescard.component';
+
+import { SidebarService } from '../../Core/Services/sidebar.service';
 import { CompanyService } from '../../Core/Services/company.service';
 import { ProfileService } from '../../Core/Services/profile.service';
-import {
-  MAT_DIALOG_DATA,
-  MatDialog,
-  MatDialogRef,
-} from '@angular/material/dialog';
-import { AllProjectsCardComponent } from "../all-projects-card/all-projects-card.component";
 import { ProjectService } from '../../Core/Services/project.service';
+
+import { Company } from '../../Core/interfaces/company/company';
 import { ProjectResult } from '../../Core/interfaces/project';
+
+import { AllcompaniescardComponent } from '../allcompaniescard/allcompaniescard.component';
+import { AllProjectsCardComponent } from '../all-projects-card/all-projects-card.component';
 import { JoinProjectComponent } from '../join-project/join-project.component';
+import { DashboardLoaderComponent } from '../../Shared/dashboard-loader/dashboard-loader.component';
 
 @Component({
   selector: 'app-all-projects',
@@ -27,51 +27,39 @@ import { JoinProjectComponent } from '../join-project/join-project.component';
     MatChipsModule,
     AllcompaniescardComponent,
     AllProjectsCardComponent,
+    DashboardLoaderComponent,
   ],
   templateUrl: './all-projects.component.html',
   styleUrl: './all-projects.component.css',
 })
 export class AllProjectsComponent {
-  constructor(
-    private sidebarService: SidebarService,
-    private _companyService: CompanyService,
-    private _ProjectService: ProjectService,
-    private _profileService: ProfileService,
-    private dialog: MatDialog,
-    private _router: Router
-  ) {}
-
-  //---------------------------------
-  joinedCompanies: Company[] = [];
-  ownedCompanies: Company[] = [];
   joinedProjects: ProjectResult[] = [];
   ownedProjects: ProjectResult[] = [];
-  companyData: Company[] = [];
-  userId!: any;
-  companyId!: any;
+
+  userId!: string;
   isSidebarCollapsed = true;
   loading = false;
 
-  showProjects: boolean = false;
+  constructor(
+    private sidebarService: SidebarService,
+    private _projectService: ProjectService,
+    private _profileService: ProfileService,
+    private _router: Router,
+    private dialog: MatDialog
+  ) {}
 
-  //---------------------------------
   ngOnInit(): void {
     this.sidebarService.isCollapsed$.subscribe((collapsed) => {
       this.isSidebarCollapsed = collapsed;
     });
-    // this.sidebarService.companyCreated$.subscribe(() => {
-    //   console.log('company is being created...');
-    //   this.getCompanies();
-    // });
-    // this.getCompanies();
-    // this.getCompaniesids();
+
     this.sidebarService.companyCreated$.subscribe(() => {
-      console.log('company is being created...');
       this.getProjects();
     });
+
     this.getProjects();
-    // this.getCompaniesIds();
   }
+
   getProjects(): void {
     this.loading = true;
 
@@ -79,22 +67,16 @@ export class AllProjectsComponent {
       next: (user) => {
         this.userId = user.id;
 
-        this._ProjectService.getAllProjects().subscribe({
+        this._projectService.getAllProjects().subscribe({
           next: (res) => {
-            if (res && res.result && res.result.length > 0) {
-              const allProjects = res.result;
+            const allProjects = res.result || [];
+            this.ownedProjects = allProjects.filter(
+              (project: any) => project.creator?.id === this.userId
+            );
 
-              this.ownedProjects = allProjects.filter(
-                (project: any) => project.creator?.id === this.userId
-              );
-
-              this.joinedProjects = allProjects.filter(
-                (project: any) => project.creator?.id !== this.userId
-              );
-
-              console.log('Owned Projects:', this.ownedProjects);
-              console.log('Joined Projects:', this.joinedProjects);
-            }
+            this.joinedProjects = allProjects.filter(
+              (project: any) => project.creator?.id !== this.userId
+            );
 
             this.loading = false;
           },
@@ -111,60 +93,18 @@ export class AllProjectsComponent {
     });
   }
 
-  // routing to every company by id
-  viewCompany(companyId: string): void {
-    this._router.navigate(['/MyDashboard/Company', companyId]);
+  viewProject(projectId: string): void {
+    this._router.navigate(['/MyDashboard/Project', projectId]);
   }
 
-  // routing to every Project by id
-  viewProject(ProjectId: string): void {
-    this._router.navigate(['/MyDashboard/Project', ProjectId]);
-  }
-
-  // get all tenants api and show all the companies i am in
-  // getCompanies(): void {
-  //   this._profileService.getProfileData().subscribe({
-  //     next: (user) => {
-  //       this.userId = user.id;
-  //       this._companyService.getAllCompanies(null).subscribe({
-  //         next: (res) => {
-  //           console.log(res);
-  //           if (res && res.result && res.result.length > 0) {
-  //             this.joinedProjects = res.result;
-  //             this.ownedCompanies = this.joinedCompanies.filter(
-  //               (company) => company.owner.id === this.userId
-  //             );
-  //           }
-  //         },
-  //         error: (err) => console.error(err),
-  //       });
-  //     },
-  //   });
-  // }
-
-  //get all companies ids
-  // getCompaniesids() {
-  //   this._companyService.getAllCompanyIds().subscribe((companyId) => {
-  //     console.log('Company IDs:', companyId);
-  //   });
-  // }
-
-  join() {
-    const dialogRef = this.dialog.open(JoinProjectComponent, {
+  join(): void {
+    this.dialog.open(JoinProjectComponent, {
       width: 'auto',
       minWidth: '40vw',
       maxWidth: '50vw',
       minHeight: '40vh',
       maxHeight: '50vh',
       disableClose: true,
-      // data: { companyId: this.company.id }, // ✅ Pass company id to modal
     });
-    // dialogRef.afterClosed().subscribe((result) => {
-    //   if (result === 'deleted') {
-    //     console.log('Company deleted successfully');
-    //     this.sidebarService.notifyCompanyDeleted();
-    //     this.router.navigate(['/MyDashboard/allcompanies']);
-    //   }
-    // });
   }
 }
